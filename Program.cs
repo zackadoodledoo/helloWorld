19f45e7b4a104bfbb3754e03bfdc015aa82f34d6
zackadoodledoo <sin12014@byui.edu>
2025-01-11 15:26:00 -0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace helloWorld
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World! My name is Zack Sinclair");
+        }
+    }
+}
